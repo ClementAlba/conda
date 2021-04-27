a86d6d8e576180a275af6a1f9b98991dddc1f8ef
Jean-Roc Morreale <jr.morreale@enoreth.net>
2021-04-27 14:49:14 +0200

    ajout des coreutils

--- create_script/pdal_env.bat
+++ create_script/pdal_env.bat
@@ -1,4 +1,5 @@
 conda create --name pdal_env
 conda activate pdal_env
-conda install -c conda-forge entwine pdal=2.2 nodejs
+conda install -c conda-forge entwine pdal nodejs
+conda install -y -c msys m2-base
 npm install ept-tools
